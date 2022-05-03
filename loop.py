476071ad41067ea9f1295d453e540c91b225c40a
shobhagavisha <shobhagavisha11@gmail.com>
2022-05-03 16:43:54 +0530

    python_progaming

---
+++ loop.py
@@ -0,0 +1,17 @@
+n=int(input("enter n value"))
+for i in range(n):
+    print("B"*i)
+
+
+n=int(input("enter n value"))
+for i in range(n):
+    print("B"*(n-i)) 
+
+n=int(input("enter n value"))
+
+for i in range(n):
+    for j in range(i+1):
+        print(j+1,end="")
+    print(" ")    
+
+

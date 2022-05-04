2afab0e92473f02d0111606cce829da4c8fb93fb
shobhagavisha <shobhagavisha11@gmail.com>
2022-05-04 12:34:32 +0530

    pathon_unpacking_program

---
+++ unpacking.py
@@ -0,0 +1,18 @@
+list1=[x for x in range(2,20)]
+print(list1)
+for x in list1:
+    print(x)
+set={1,2,3,4,5,6}
+for x in set:
+    print(x)
+dict={1:'shobha',2:'python',3:7.7,4:'c'}
+for x in dict:
+    print(dict.items())
+    print(dict.values())
+    print(dict[2])
+str="Shoha"
+print(list(str)) 
+print(tuple(str))
+print(str.upper())
+print(str.isupper())
+print(str.islower())
